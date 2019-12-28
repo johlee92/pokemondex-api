ce78d53c889434877ba71d3871cabb1e221595ec
JY <johnathanlee72@hotmail.com>
2019-12-27 17:28:37 -0700

    fixed security vulnerability on API token

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganSetting));
 
-
 const PORT = process.env.PORT || 8000;
-
 
 app.use((error,req, res, next) => {
   let response;
@@ -30,9 +26,17 @@ app.listen(PORT, () => {
 });
 
 app.use(function validateBearerToken(req, res, next) {
-  // console.log('validate bearer token middleware')
-  // move to the next middleware
-  next()
+  
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  console.log(apiToken);
+  console.log(authToken.split(' ')[1]);
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request'});
+  }
+
+  next();
 })
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
