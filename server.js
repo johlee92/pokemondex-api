da03c595d44acb3765455870767c083d42314696
JY <johnathanlee72@hotmail.com>
2019-12-27 17:33:31 -0700

    removed console logs

--- server.js
+++ server.js
@@ -26,11 +26,9 @@ app.listen(PORT, () => {
 });
 
 app.use(function validateBearerToken(req, res, next) {
-  
+
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-  console.log(apiToken);
-  console.log(authToken.split(' ')[1]);
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request'});
